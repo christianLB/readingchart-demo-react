328c3e7f113d4b62741fd117f6844fbccbf9e6cb
christianLB <k2600x@gmail.com>
2019-01-10 02:18:07 +0100

    first production build

---
+++ build/precache-manifest.b6a2c6f753c1ee2aaef8dc6acab09c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd5f9a3a6bd61905cc10",
+    "url": "/static/js/main.bd5f9a3a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bd5f9a3a6bd61905cc10",
+    "url": "/static/css/main.ce7b0e51.chunk.css"
+  },
+  {
+    "revision": "e1216aef3f04dfa4d8b8ba6877b80905",
+    "url": "/index.html"
+  }
+];
